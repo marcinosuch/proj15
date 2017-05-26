d1c8167e544dd192281e364018d0445cdf62d863
marcinosuch <mar.osuch@gmail.com>
2017-05-26 09:50:43 +0200

    warunki

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+// scripts.js
+
+
+var a = prompt('Enter a');
+	b = prompt('Enter b');
+	value = (a * a) + (2 * a * b) - (b * b)
+	tA = alert('(a * a) + (2 * a * b) - (b * b) =  ' + value);
+	console.log('(a * a) + (2 * a * b) - (b * b) =  ' + value);
+
+if (value > 0) {
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+} else {
+	console.log('zero');}
+
+/*
+
+
+
+var selectedTab = 'details';
+
+if (selectedTab == 'details') {
+    console.log('You selected details tab');
+} else if (selectedTab == 'comments') {
+    console.log('You selected comments tab');
+} else {
+    console.log('You selected product description tab');
+}
+
+*/
+
+
+
+
+
+/*
+var a = prompt('Enter base of triangle');
+	h = prompt('Enter height of triangle');
+	triangleArea = a * h / 2
+	tA = alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	
+
+
+
+	name = prompt('Enter your name');
+	alert('Hello, ' + name);
+	console.log('Hello, ' + name);
+
+*/
+
+
+
